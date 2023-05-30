eaed14e77e09b8ff3eec3099a6dc9aceadfb5bd3
chyhhwen <chyhhwen@gmail.com>
2023-05-31 05:55:37 +0800

    test

--- index.php
+++ index.php
@@ -5,6 +5,115 @@
     {
         switch(@g('page'))
         {
+            case "cart":
+                echo
+                '
+                    <html>
+                    <head>
+                        <link rel="stylesheet" href="./css/cart.css">
+                        <script type=\'text/javascript\' src=\'./js/cart.js\'></script> 
+                        <meta charset="UTF-8">
+                        <title>家佑好皮書店</title>
+                    </head>
+                    <body>
+                    <nav>
+                        <span style="padding-right:75vw;">'. $_COOKIE["user"] .'</span>
+                        <a href="index.php">首頁</a>
+                    </nav>
+                    <div class="state">
+                        <div class="title">
+                            <h2>目前階段</h2>
+                        </div>
+                ';
+                echo'<script>window.onload = () =>{';
+                switch(@g('stage')) 
+                {
+                    case "1":
+                        echo'details();';
+                    break;
+                    case "2":
+                        echo'write();';
+                    break;
+                    case "3":
+                        echo'check();';
+                    break;
+                    case "4":
+                        echo'usepay();';
+                    break;
+                    case "5":
+                        echo'curret();';
+                    break;
+                    default:
+                        echo'details();';
+                    break;
+                }               
+                echo'}</script>';    
+                echo
+                '                
+                    <div class="present">
+                        <div class="stage">
+                            <a href="javascript:details()" id="one">
+                                <h3>1.確認明細</h3>
+                            </a>
+                        </div>
+                        <div class="stage">
+                            <a href="javascript:write()" id="two">
+                                <h3>2.填寫資料</h3>
+                            </a>
+                        </div>
+                        <div class="stage">
+                            <a href="javascript:check()" id="three">
+                                <h3>3.訂單確認</h3>
+                            </a>
+                        </div>
+                        <div class="stage">
+                            <a href="javascript:usepay()" id="four">
+                                <h3>4.進行付款</h3>
+                            </a>
+                        </div>
+                        <div class="stage">
+                            <a href="javascript:curret()" id="five">
+                                <h3>5.完成訂購</h3>
+                            </a>
+                        </div>
+                    </div>
+                    </div>
+                    <div id="list"></div>
+                    <div class="notice">
+                        <div class="title">
+                            <h2>注意事項</h2>
+                        </div>
+                        <div class="test">
+                            <h3>訂購缺書書籍的訂單</h3>
+                            <h3>當您的訂單中同時包含缺書中與有現貨之書籍時，將於書籍到齊後才一併出貨及寄出，若您不願等待缺書中之書籍，建議將有現貨與缺貨籍各別下訂單。</h3>
+                            <h3>缺貨書籍需等候時間</h3>
+                            <h3>缺貨之書籍，根據供貨來源不同，約可分為下列四種：</h3>
+                            <h3>中文書：供應商有現貨，約 3~7個工作天左右可到貨</h3>
+                            <h3>非超皮代理進口之原文書：若代理商有現貨，約 5~8個工作天左右到貨</h3>
+                            <h3>超皮代理進口之原文書：若國外供應商有現貨，約 2週~4週左右到貨</h3>
+                            <h3>代購書籍：若國外廠商可供貨，約 2週~4週左右到貨</h3>
+                            <h3>以上到貨時間若因無法控制之因素而延遲到貨，我們將儘速通知您，您可自行決定是否保留訂單繼續等候或是取消訂單。</h3>
+                            <h3>若書籍有無法供貨情形發生，我們會打電話通知您。</h3>
+                            <h3>選擇匯款或劃撥之訂單</h3>
+                            <h3>下單後請別先行匯款，請在收到我們的匯款通知信後，再去匯款即可，並通知我們您的匯款資訊，以加速訂單處理速度。</h3>
+                            <h3>家佑真的好皮</h3>
+                            <h3>超皮的啦</h3>
+                        </div>
+                    </div>
+                    <footer>
+                        <span>
+                            <a href="#">客後服務</a>|  
+                            <a href="#">連絡我們 </a>| 
+                            <a href="#">隱私權政策 </a>| 
+                            <a href="#">服務條款</a>
+                        </span><br>
+                        <span>家佑真的好皮訊圖書有限公司</span><br>
+                        <span>營業人統一編號 123456789</span>
+                    </footer>
+                    </body>
+                    </html>
+                ';
+            break;    
             case "chinese":
                 echo
                 '
@@ -18,7 +127,7 @@
                     <nav>
                         <span style="padding-right:65vw;">'. $_COOKIE["user"] .'</span>
                         <a href="index.php">首頁</a>
-                        <a href="javascript:alert(\'請先登入\')">購物車</a>
+                        <a href="index.php?page=cart">購物車</a>
                     </nav>
                 ';      
                 $data = file_get_contents( './lib/sql.json' );
@@ -88,7 +197,7 @@
                     <nav>
                         <span style="padding-right:65vw;">'. $_COOKIE["user"] .'</span>
                         <a href="index.php">首頁</a>
-                        <a href="javascript:alert(\'請先登入\')">購物車</a>
+                        <a href="index.php?page=cart">購物車</a>
                     </nav>
                 ';      
                 $data = file_get_contents( './lib/sql.json' );
@@ -153,6 +262,7 @@
                     <html>
                     <head>
                         <link rel="stylesheet" href="./css/index.css"> 
+                        <script type=\'text/javascript\' src=\'./js/cart.js\'></script> 
                         <meta charset="UTF-8">
                         <title>家佑好皮書店</title>
                     </head>
@@ -160,7 +270,7 @@
                         <nav>
                             <span style="padding-right:75vw;">'. $_COOKIE["user"] .'</span>
                             <a href="index.php">首頁</a>
-                            <a href="javascript:alert(\'請先登入\')">購物車</a>
+                            <a href="index.php?page=cart">購物車</a>
                         </nav>
                 ';
                 /*intro*/
@@ -206,11 +316,13 @@
                                     <div class="text">
                                         <h2>作者:'. $writer .'</h2>
                                         <h2>出版商:'. $publisher .'</h2>
-                                        <h2>售價:'. $row[$db['book_field'][3]] .'</h2>
-                                        <h2>定價:'. round(intval($row[$db['book_field'][3]]) * 0.8) .'</h2>
+                                        <h2>售價:$'. $row[$db['book_field'][3]] .'</h2>
+                                        <h2>定價:$'. round(intval($row[$db['book_field'][3]]) * 0.8) .'</h2>
                                         <h2>語言:'. $row[$db['book_field'][5]] .'</h2>
                                         <h2>剩餘:'. $row[$db['book_field'][4]] .'</h2>
-                                        <input type="submit" value="加入購物車">
+                                        <input type="hidden"  id="inputname" value="'. $row[$db['book_field'][2]] .'">
+                                        <input type="hidden"  id="inputprice" value="'. round(intval($row[$db['book_field'][3]]) * 0.8) .'">
+                                        <input type="submit" onclick="addItem(\'#inputname\', \'#inputprice\', \''. $row[$db['book_field'][1]] .'\')" value="加入購物車">
                                     </div>
                                 </div>
                             </div>
@@ -254,7 +366,6 @@
                 <html>
                 <head>
                     <link rel="stylesheet" href="./css/index.css">
-                    <script type=\'text/javascript\' src=\'./js/index.js\'></script>
                     <script type=\'text/javascript\' src=\'./js/client.js\'></script> 
                     <meta charset="UTF-8">
                     <title>家佑好皮書店</title>
@@ -263,7 +374,7 @@
                     <nav>
                         <span style=padding-right:70%;>'. $_COOKIE["user"] .'</span>
                         <a href="javascript:reset(\'#test\')">首頁</a>
-                        <a href="./cart.html">購物車</a>
+                        <a href="index.php?page=cart">購物車</a>
                     </nav>
                     <div id="test"></div>
                     </div>
@@ -590,7 +701,6 @@
                                         <h2>定價:'. round(intval($row[$db['book_field'][3]]) * 0.8) .'</h2>
                                         <h2>語言:'. $row[$db['book_field'][5]] .'</h2>
                                         <h2>剩餘:'. $row[$db['book_field'][4]] .'</h2>
-                                        <input type="submit" value="加入購物車">
                                     </div>
                                 </div>
                             </div>
--- js/cart.js
+++ js/cart.js
@@ -1,65 +1,93 @@
-var requestURL = './lib/fix.json';
-var request = new XMLHttpRequest();
-var list = [];
-var liname = [];
-var client = [];
-var pay = [];
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
-request.onload = function() 
+var list = ["1.確認明細","2.填寫資料","3.訂單確認","4.進行付款","5.完成訂購"];
+var client=["姓名","地址"];
+var pay = ["姓名","卡號","日期","OTP"];
+const shop = {
+	list: {}, 
+    add: function (ID, Name, Price, Amount) 
+    {
+		if (this.list.hasOwnProperty(ID)) 
+        {
+			this.list[ID]["Amount"] += Amount;
+			this.list[ID]["Price"] = Price * this.list[ID]["Amount"];
+		} 
+        else 
+        {
+			shop.init(ID, Name, Price, Amount);
+		}
+	}, 
+    init: function (ID, Name, Price, Amount, overwrite = true) 
+    {
+		if (overwrite) this.list[ID] = {"Name": Name, "Price": Price, "Amount": Amount};
+	}, 
+    del: function (ID) 
+    {
+		delete this.list[ID];
+	}, 
+    total: () => 
+    {
+		let price = 0;
+		Object.entries(shop.list).forEach(([key, value]) => 
+        {
+			price += value["Price"];
+		});
+		return price;
+	}
+};
+
+/**
+ * 物品刪除
+ * @param key
+ */
+delItem = (key) =>
 {
-    list = request.response.list;
-    liname = request.response.liname;
-    client = request.response.client;
-    pay = request.response.pay;
+	shop.del(key);
 }
-window.onload =()=>
+
+/**
+ * 呼叫資料
+ * @param ItemID
+ */
+callItem = (ItemID) =>
 {
+
+}
+
+/**
+ * 新增物品
+ * @param e1 物品名稱
+ * @param e2 物品價錢
+ * @param e3 物品ID
+ */
+addItem = (e1, e2, e3) => 
+{
+	let inputname_el = document.querySelector(e1);
+	let inputname = inputname_el.value;
+	let inputprice_el = document.querySelector(e2);
+	let inputprice = inputprice_el.value;
+	shop.add(e3,inputname,inputprice,1);
+    alert('新增成功');
+}
+set = (s) =>
+{
+    document.querySelector("#one").style = "color: black";
+    document.querySelector("#two").style = "color: black";
+    document.querySelector("#three").style = "color: black";
+    document.querySelector("#four").style = "color: black";
+    document.querySelector("#five").style = "color: black";
+    document.querySelector(s).style = "color: #bb5a3a;";
+}
+curret = () =>
+{
+    set('#five');
     var view="";
-    view += "<div id=title><h2>1.確認明細</h2>";
+    view += "<div id=title><h2>"+ list[4] +"</h2>";
     view += "</div><div id=item>";
-    view += "<h2>123</h2></div>";
-    view += "<div id=input>";
-    view += "<input type=submit value=確認></div>";
-    document.getElementById('list').innerHTML = view;
-}
-details = () =>
-{
-    var view="";
-    view += "<div id=title><h2>"+ list[0] +"</h2>";
-    view += "</div><div id=item>";
-    view += "<h2>123</h2></div>";
-    view += "<div id=input>";
-    view += "<input type=submit value=確認></div>";
-    document.getElementById('list').innerHTML = view;
-}
-write = () =>
-{
-    var view="";
-    view += "<div id=title><h2>"+ list[1] +"</h2></div>";
-    for(var i=0;i<2;i++)
-    { 
-        view += "<div id=input>";
-        view += "<h2>"+ client[i] +"</h2>";
-        view += "<input type=text></div>";
-    }
-    view += "<div id=input>";
-    view += "<input type=submit value=確認></div>";
-    document.getElementById('list').innerHTML = view;
-}
-check = () =>
-{
-    var view="";
-    view += "<div id=title><h2>"+ list[2] +"</h2>";
-    view += "</div><div id=item>";
-    view += "<h2>123</h2></div>";
-    view += "<div id=input>";
-    view += "<input type=submit value=確認></div>";
+    view += "<h2>訂單已完成</h2></div>";
     document.getElementById('list').innerHTML = view;
 }
 usepay = () =>
 {
+    set('#four');
     var view="";
     view += "<div id=title><h2>"+ list[3] +"</h2></div>";
     for(var i=0;i<4;i++)
@@ -69,14 +97,49 @@ usepay = () =>
         view += "<input type=text></div>";
     }
     view += "<div id=input>";
-    view += "<input type=submit value=確認></div>";
+    view += "<input type=submit value=確認 ";
+    view +=  "onclick=\"location.href=\'index.php?page=cart&stage=5\'\"></div>";
     document.getElementById('list').innerHTML = view;
 }
-curret = () =>
+check = () =>
 {
+    set('#three');
     var view="";
-    view += "<div id=title><h2>"+ list[4] +"</h2>";
+    view += "<div id=title><h2>"+ list[2] +"</h2>";
     view += "</div><div id=item>";
-    view += "<h2>訂單已完成</h2></div>";
+    view += "<h2>123</h2></div>";
+    view += "<div id=input>";
+    view += "<input type=submit value=確認 ";
+    view +=  "onclick=\"location.href=\'index.php?page=cart&stage=4\'\"></div>";
     document.getElementById('list').innerHTML = view;
 }
+write = () =>
+{
+    set('#two');
+    var view="";
+    view += "<div id=title><h2>"+ list[1] +"</h2></div>";
+    for(var i=0;i<2;i++)
+    { 
+        view += "<div id=input>";
+        view += "<h2>"+ client[i] +"</h2>";
+        view += "<input type=text></div>";
+    }
+    view += "<div id=input>";
+    view += "<input type=submit value=確認 ";
+    view +=  "onclick=\"location.href=\'index.php?page=cart&stage=3\'\"></div>";
+    document.getElementById('list').innerHTML = view;
+}
+details = () =>
+{
+    set('#one');
+    var view="";
+    view += "<div id=title><h2>"+ list[0] +"</h2>";
+    view += "</div><div id=item>";
+    view += "<h2>123</h2></div>";
+    view += "<div id=input>";
+    view += "<input type=submit value=確認 ";
+    view +=  "onclick=\"location.href=\'index.php?page=cart&stage=2\'\"></div>";
+    document.getElementById('list').innerHTML = view;
+}
+
+
