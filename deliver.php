306790bc4a744aa0c47e532729cda2ee23d1239d
chyhhwen <chyhhwen@gmail.com>
2023-05-31 13:34:39 +0800

    test

--- deliver.php
+++ deliver.php
@@ -3,4 +3,12 @@
     $n = p('name');
     $c = p('card');
     $a = p('access');
+    $o = $_COOKIE['order'];
+    $u = $_COOKIE['token'];
+    $data = file_get_contents( './lib/sql.json' );
+    $db = json_decode( $data, true );
+    $db_data = ['',$o, $u,$n,$c,$a,$GLOBALS["time"]];
+    $sql = "INSERT INTO ". $db['dbname6'] ." VALUES (?, ?, ?, ?, ?, ?, ?)";
+    add($db['db'],$db_data,$sql);
+    ref([0,'index.php?page=cart&stage=4']);
 ?>
